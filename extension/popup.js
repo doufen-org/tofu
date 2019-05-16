9a6543ad9ba644c68bf7862c59e73352524819c4
tabris17 <tabris17@fournoas.com>
2019-05-16 21:44:22 +0800

    #

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,7 @@
-document.querySelector('.menu').addEventListener('click', event => {
-
+document.querySelector('.menu').addEventListener('click', function (event) {
+    console.log(event);return;
+    let node = event.target;
+    while (!node.matches('')) {
+        node = node.parentNode;
+    }
 })
