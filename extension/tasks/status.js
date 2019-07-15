af4b22105e2da5a3b59d5d7e134befa50c053d2c
doufen-org <admin@doufen.org>
2019-07-15 13:55:22 +0800

    #

--- extension/tasks/status.js
+++ extension/tasks/status.js
@@ -3,30 +3,57 @@ import {TaskError, Task} from '../service.js';
 
 
 const URL_TIMELINE = 'https://m.douban.com/rexxar/api/v2/status/user_timeline/{uid}?max_id={maxId}&ck={ck}&for_mobile=1';
+const URL_STATUS = 'https://m.douban.com/rexxar/api/v2/status/{id}?ck={ck}&for_mobile=1';
 
 
 export default class Status extends Task {
+    async fetchStatusFulltext(id) {
+        let url = URL_STATUS
+            .replace('{ck}', this.session.cookies.ck)
+            .replace('{id}', id);
+        let response = await this.fetch(url, {headers: {'X-Override-Referer': 'https://m.douban.com/mine/statuses'}});
+        if (response.status != 200) {
+            throw new TaskError('豆瓣服务器返回错误');
+        }
+        return await response.json();
+    }
+
     async run() {
+        let version = this.jobId;
         this.total = this.session.userInfo.statuses_count;
+        let lastStatusId = '';
+        await this.storage.transaction('rw', this.storage.table('version'), async () => {
+            let verTable = this.storage.table('version');
+            let row = await verTable.get('status');
+            if (row) {
+                lastStatusId = row.lastId;
+                await verTable.update('status', {version: version, updated: Date.now()});
+            } else {
+                await verTable.add({table: 'status', version: version, updated: Date.now()});
+            }
+        })
 
         let baseURL = URL_TIMELINE
             .replace('{ck}', this.session.cookies.ck)
             .replace('{uid}', this.session.userId);
 
-        let lastRow = await this.storage.status.orderBy('id').limit(1).first();
-        let maxId = lastRow ? lastRow.id : '', count;
+        let count;
         do {
-            let response = await this.fetch(baseURL.replace('{maxId}', maxId), {headers: {'X-Override-Referer': 'https://m.douban.com/mine/statuses'}});
+            let response = await this.fetch(baseURL.replace('{maxId}', lastStatusId), {headers: {'X-Override-Referer': 'https://m.douban.com/mine/statuses'}});
             if (response.status != 200) {
                 throw new TaskError('豆瓣服务器返回错误');
             }
             let json = await response.json();
             count = json.items.length;
             for (let item of json.items) {
+                console.log(item);
                 let status = item.status;
                 item.id = parseInt(status.id);
                 item.created = Date.now();
-                maxId = status.id;
+                lastStatusId = status.id;
+                if (status.text.length == 140 && status.text.substr(-3, 3) == '...') {
+                    item.status = await this.fetchStatusFulltext(lastStatusId);
+                }
                 try {
                     await this.storage.status.add(item);
                 } catch (e) {
@@ -37,9 +64,10 @@ export default class Status extends Task {
                     }
                     throw e;
                 }
+                await this.storage.table('version').update('status', { lastId: item.id });
                 this.step();
             }
-        } while (count > 0 || (maxId = '') == '');
+        } while (count > 0 || (lastStatusId = '') == '');
         this.complete();
     }
 
