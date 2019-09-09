379294bcb7356b8e2e53be8068bc091be33309e4
doufen-org <admin@doufen.org>
2019-09-09 18:05:55 +0800

    #

--- extension/tasks/migrate/interest.js
+++ extension/tasks/migrate/interest.js
@@ -2,7 +2,20 @@
 import {Task} from '../../service.js';
 
 
-const URL_MARK_INTEREST = 'https://www.douban.com/j/contact/addtoblacklist';
+const URL_INTEREST = {
+    movie: 'https://movie.douban.com/j/subject/{subject_id}/interest',
+    music: 'https://muisc.douban.com/j/subject/{subject_id}/interest',
+    book: 'https://book.douban.com/j/subject/{subject_id}/interest',
+    game: 'https://www.douban.com/j/ilmen/thing/{subject_id}/interest',
+    drama: 'https://www.douban.com/j/location/drama/{subject_id}/interest',
+};
+
+const MARKS = {
+    mark: 'wish',
+    doing: 'do',
+    done: 'collect',
+};
+
 const PAGE_SIZE = 100;
 
 
@@ -14,15 +27,38 @@ export default class Note extends Task {
         }
 
         let postData = new URLSearchParams();
+        postData.append('ck', this.session.cookies.ck);
+        postData.append('foldcollect', 'F');
+
         let pageCount = Math.ceil(this.total / PAGE_SIZE);
         for (let i = 0; i < pageCount; i ++) {
             let rows = await this.storage.interest
                 .offset(PAGE_SIZE * i).limit(PAGE_SIZE)
                 .toArray();
             for (let row of rows) {
+                let interest = row.interest;
+                postData.set('rating', interest.rating || '');
+                postData.set('interest', MARKS[row.status]);
+                postData.set('tags', interest.tags ? interest.tags.join(' ') : '');
+                postData.set('comment', interest.comment + ' @' + interest.create_time);
+
+                let response = await this.fetch(URL_INTEREST[row.type].replace('{subject_id}', row.subject), {
+                    headers: {
+                        'X-Override-Referer': 'https://www.douban.com/',
+                        'X-Requested-With': 'XMLHttpRequest',
+                        'X-Override-Origin': 'https://www.douban.com',
+                    },
+                    method: 'POST',
+                    body: postData,
+                });
+                let result = await response.json();
                 this.step();
             }
         }
         this.complete();
+    }
+
+    get name() {
+        return '标记影音书游剧';
     }
 }
